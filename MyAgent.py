017f29bb573cf4b9c4beda46d31ac4b2732a489c
Hassan Sheraz <hsnshrz@gmail.com>
2025-06-28 17:06:15 -0500

    Updated MyAgent.py to test using MinMax

--- MyAgent.py
+++ MyAgent.py
@@ -1,58 +1,18 @@
+
+import random
 from Game2048 import *
 
-class Player:
+class Player(BasePlayer):
     def __init__(self, timeLimit):
-        self.timeLimit = timeLimit
+        BasePlayer.__init__(self, timeLimit)
 
-    def findMove(self, state):
-        moves = state.actions()
-        best = None
-        bestScore = float('-inf')
-        for move in moves:
-            nextState, _ = state.result(move)
-            score = self.evaluate(nextState)
-            if score > bestScore:
-                bestScore = score
-                best = move
-        return best
+    def findMove(self, board):
+        actions = board.actions()
+        a = random.choice(actions)
+        self.setMove(a)
 
-    def evaluate(self, state):
-        board = state._board
-        empty = sum(1 for r in range(4) for c in range(4) if board[r][c] == 0)
-        smooth = self.smoothness(board)
-        monotonic = self.monotonicity(board)
-        maxTile = max(max(row) for row in board)
-        return empty * 200 + smooth + monotonic + maxTile * 2
+    def loadData(self, filename):
+        pass
 
-    def smoothness(self, board):
-        score = 0
-        for r in range(4):
-            for c in range(3):
-                if board[r][c] and board[r][c+1]:
-                    score -= abs(board[r][c] - board[r][c+1])
-        for r in range(3):
-            for c in range(4):
-                if board[r][c] and board[r+1][c]:
-                    score -= abs(board[r][c] - board[r+1][c])
-        return score
-
-    def monotonicity(self, board):
-        totals = [0, 0, 0, 0]
-
-        # rows
-        for r in range(4):
-            for c in range(3):
-                if board[r][c] > board[r][c+1]:
-                    totals[0] += board[r][c] - board[r][c+1]
-                else:
-                    totals[1] += board[r][c+1] - board[r][c]
-
-        # cols
-        for c in range(4):
-            for r in range(3):
-                if board[r][c] > board[r+1][c]:
-                    totals[2] += board[r][c] - board[r+1][c]
-                else:
-                    totals[3] += board[r+1][c] - board[r][c]
-
-        return -min(totals[0], totals[1]) - min(totals[2], totals[3])
+    def saveData(self, filename):
+        pass
